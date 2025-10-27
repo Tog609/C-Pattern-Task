58dd6598ce5afc89fcbc3c86c6ed69646dd3b42e
IVAN LEMESH <lemesh.ivan@student.ehu.lt>
2025-10-27 12:44:50 +0200

    add eagle initialization Singleton - the simplest option

---
+++ SingletonRealization - 2 parts/SingleTonRealization/Logger.cs
@@ -0,0 +1,16 @@
+﻿sealed class Logger
+{
+    private static Logger _instance = new Logger();
+
+    public static Logger Instance
+    {
+        get { return _instance; }
+    }
+
+    private Logger() { }
+
+    public void SendLogs(string logMessage)
+    {
+        Console.WriteLine(logMessage);
+    }
+}
---
+++ SingletonRealization - 2 parts/SingleTonRealization/Program.cs
@@ -0,0 +1,7 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Logger.Instance.SendLogs("Abobys");
+    }
+}
