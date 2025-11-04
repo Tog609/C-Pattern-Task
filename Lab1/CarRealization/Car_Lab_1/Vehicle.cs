b8d4adc7202507f0493535050996181b78f1839c
IVAN LEMESH <lemesh.ivan@student.ehu.lt>
2025-11-04 15:22:51 +0200

    AAaaaaa
    Big issue

--- Lab1/CarRealization/Car_Lab_1/Vehicle.cs
+++ Lab1/CarRealization/Car_Lab_1/Vehicle.cs
@@ -8,12 +8,18 @@ public class Vehicle : Transport
         {2, "back"}
     };
 
-    private string? _vehicleClass;
+    private Dictionary<int, string> _class = new Dictionary<int, string>
+    {
+        {1,"hatchback"},
+        {2, "sedan"},
+        {3, "coupe"}
+    };
 
     private Color _color;
 
+
     public sealed override string ToString()
     {
-        return $"Vehicle: Weight={_weight}, Length={_length}, MaxSpeed={_maxSpeed}, Class={_vehicleClass}, Color = {_color} WheelDrive={string.Join(", ", _wheelDrive)}";
+        return $"Vehicle: Weight={_weight}, Length={_length}, MaxSpeed={_maxSpeed}, Class={_class}, Color = {_color} WheelDrive={string.Join(", ", _wheelDrive)}";
     }
 }
