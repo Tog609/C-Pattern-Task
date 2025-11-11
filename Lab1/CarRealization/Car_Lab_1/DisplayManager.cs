eecfc7fd59bf3cf0fd6750ea9393189462a9b10e
IVAN LEMESH <lemesh.ivan@student.ehu.lt>
2025-11-11 12:25:38 +0200

    LastFix

---
+++ Lab1/CarRealization/Car_Lab_1/DisplayManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Car_Lab_1
+{
+    internal sealed class DisplayManager
+    {
+        private static readonly DisplayManager _instance = new DisplayManager();
+
+        public static DisplayManager Instance => _instance;
+
+        private DisplayManager() { }
+
+        public void ShowSection(string title)
+        {
+            Console.WriteLine($"=== {title.ToUpper()} ===");
+        }
+
+        public void ShowTransport(params Transport[] transports)
+        {
+            foreach (var transport in transports)
+            {
+                Console.WriteLine(transport);
+            }
+            Console.WriteLine();
+        }
+
+        public void ShowMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
--- Lab1/CarRealization/Car_Lab_1/Program.cs
+++ Lab1/CarRealization/Car_Lab_1/Program.cs
@@ -5,38 +5,36 @@ namespace Car_Lab_1
 {
     internal class Program
     {
+        private const string _cargoSection = "CARGO";
+
+        private const string _vehicleSection = "VEHICLES";
+
+        private const string _tankSection = "TANKS";
         static void Main(string[] args)
         {
+            var display = DisplayManager.Instance;
+
             VehicleFactory vehicleFactory = new VehicleFactory();
-
-            Console.WriteLine("=== VEHICLES ===");
-            Transport audi = vehicleFactory.CreateTransport(1500, 4.5f, 240, Color.Red, 1, 2);   // sedan, front
-            Transport honda = vehicleFactory.CreateTransport(1400, 4.4f, 220, Color.Blue, 2, 1); // hatchback, back
-            Transport tesla = vehicleFactory.CreateTransport(1800, 4.7f, 260, Color.Black, 1, 3); // coupe, front
-            Console.WriteLine(audi);
-            Console.WriteLine(honda);
-            Console.WriteLine(tesla);
-            Console.WriteLine();
-
             CargoFactory cargoFactory = new CargoFactory();
+            TankFactory tankFactory = new TankFactory();
+
+            display.ShowSection(_vehicleSection);
+            Transport audi = vehicleFactory.CreateTransport(1500, 4.5f, 240, Color.Red, 1, 2);
+            Transport honda = vehicleFactory.CreateTransport(1400, 4.4f, 220, Color.Blue, 2, 1);
+            Transport tesla = vehicleFactory.CreateTransport(1800, 4.7f, 260, Color.Black, 1, 3);
+            display.ShowTransport(audi, honda, tesla);
+
+            display.ShowSection(_cargoSection);
             Transport volvo = cargoFactory.CreateTransport(8000, 7.5f, 140, 20, 600, 3);
             Transport man = cargoFactory.CreateTransport(7500, 7.0f, 130, 18, 550, 3);
             Transport scania = cargoFactory.CreateTransport(8200, 7.8f, 150, 22, 650, 4);
-            Console.WriteLine("=== CARGO ===");
-            Console.WriteLine(volvo);
-            Console.WriteLine(man);
-            Console.WriteLine(scania);
-            Console.WriteLine();
+            display.ShowTransport(volvo, man, scania);
 
-            TankFactory tankFactory = new TankFactory();
+            display.ShowSection(_tankSection);
             Transport abrams = tankFactory.CreateTransport(62000, 9.8f, 72, 120f, 10, 4);
             Transport merkava = tankFactory.CreateTransport(65000, 9.5f, 64, 120f, 8, 4);
             Transport tiger = tankFactory.CreateTransport(57000, 8.6f, 45, 88f, 6, 5);
-            Console.WriteLine("=== TANKS ===");
-            Console.WriteLine(abrams);
-            Console.WriteLine(merkava);
-            Console.WriteLine(tiger);
-            Console.WriteLine();
+            display.ShowTransport(abrams, merkava, tiger);
         }
     }
 }
